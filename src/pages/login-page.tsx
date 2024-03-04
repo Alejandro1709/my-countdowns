8f41de95f6e5ba07f089a264fadff4cbc6e044de
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-03-04 17:16:43 -0500

    Move env to .locals

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/layout";
 function LoginPage() {
   return (
     <Layout title="My Countdowns | Login">
-      <h1>Please</h1>
+      <h1>Please Login</h1>
     </Layout>
   );
 }
