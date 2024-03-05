1121f2ba8077fe04f585228d46a3c352bee90105
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-03-04 19:06:21 -0500

    Add: User Login and profile picture

--- src/components/account/account-button.tsx
+++ src/components/account/account-button.tsx
@@ -9,6 +9,7 @@ import {
 import { Button } from "../ui/button";
 import { type Session } from "next-auth";
 import { signOut } from "next-auth/react";
+import Image from "next/image";
 
 function AccountButton({ session }: { session: Session }) {
   return (
@@ -17,7 +18,18 @@ function AccountButton({ session }: { session: Session }) {
         <Button variant="secondary">Account</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>{session.user.name}</DropdownMenuLabel>
+        <DropdownMenuLabel className="flex flex-row gap-2">
+          {session.user.image ? (
+            <Image
+              className="rounded-md"
+              height={20}
+              width={20}
+              src={session.user.image}
+              alt={session.user.name ?? "NA"}
+            />
+          ) : null}
+          {session.user.name}
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         {/* <DropdownMenuItem>Profile</DropdownMenuItem> */}
         <DropdownMenuItem onClick={() => signOut()}>Logout</DropdownMenuItem>
